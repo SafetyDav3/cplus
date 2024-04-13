4a109be5a5845d881f71a4f76234f77b65466752
David Williams <85596817+SafetyDav3@users.noreply.github.com>
2024-04-13 14:07:50 -0500

    rand number gen with seed number

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+// Random number generator
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    srand(569876);       // Seed the random number generator
+    int number = rand(); // Generate a random number
+    cout << number;      // Output the random number
+    return 0;            // End the program
 }
