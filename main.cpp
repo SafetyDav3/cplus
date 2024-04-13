7bf65006b84786bad8ddd762462866d7d3872c9b
David Williams <85596817+SafetyDav3@users.noreply.github.com>
2024-04-13 14:21:49 -0500

    random number based on time from Jan,1,1970

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime> // Get time originating on Jan 1 1970
 
 using namespace std;
 
 // Random number generator
 int main()
 {
-    srand(569876);       // Seed the random number generator
-    int number = rand(); // Generate a random number
-    cout << number;      // Output the random number
-    return 0;            // End the program
+    srand(time(0));               // Seed the random number generator with the current time
+    int number = rand() % 10'000; // Generate a random number between 0 and 9999
+    cout << number;               // Output the random number
+    return 0;                     // End the program
 }
