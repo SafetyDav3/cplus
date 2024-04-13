e4cfb00c9c673be52be5d7ca2136dd9e8e829adf
David Williams <85596817+SafetyDav3@users.noreply.github.com>
2024-04-13 13:45:01 -0500

    back to hello world

--- main.cpp
+++ main.cpp
@@ -2,51 +2,6 @@
 
 int main()
 {
-    using namespace std;
-
-    char decision2;
-
-    do
-    {
-        double var1, var2;
-        char decision;
-
-        cout << "Enter first number: " << endl;
-        cin >> var1;
-
-        cout << "Enter second number: " << endl;
-        cin >> var2;
-
-        cout << "What do you want to do with those numbers?" << endl;
-        cout << "+ - add" << endl;
-        cout << "- - subtract" << endl;
-        cout << "* - multiply" << endl;
-        cout << "/ - divide" << endl;
-
-        cin >> decision;
-
-        switch (decision)
-        {
-        case '+':
-            cout << var1 << " + " << var2 << " = " << (var1 + var2) << endl;
-            break;
-        case '-':
-            cout << var1 << " - " << var2 << " = " << (var1 - var2) << endl;
-            break;
-        case '*':
-            cout << var1 << " * " << var2 << " = " << (var1 * var2) << endl;
-            break;
-        case '/':
-            if (var2 != 0)
-                cout << var1 << " / " << var2 << " = " << (var1 / var2) << endl;
-            else
-                cout << "You can't divide by 0" << endl;
-            break;
-        default:
-            cout << "You typed wrong character";
-        }
-
-        cout << "Do you want to continue that program? (Y/N)" << endl;
-        cin >> decision2;
-    } while (decision2 == 'y' || decision2 == 'Y');
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
